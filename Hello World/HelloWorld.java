67fbf165b432f195519b9729e6c7ca62588e95a4
maclamour <maclamour@gmail.com>
2023-03-16 09:30:42 -0400

    operations.java

--- Hello World/HelloWorld.java
+++ Hello World/HelloWorld.java
@@ -3,6 +3,20 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+
+    int totalSugar = 0;
+
+    int sugar = 15;
+
+    int maxSugar = 10;
+
+    if (sugar <= maxSugar){
+        totalSugar += sugar;
+        System.out.println(totalSugar);
+    }else {
+        System.out.println("We only allow " + maxSugar + " tsp's of sugar per glass ");
+    }
+
     }
 
 }
