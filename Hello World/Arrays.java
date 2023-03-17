8a35b5f25edb86461e69d743af59ac957c7743e8
maclamour <maclamour@gmail.com>
2023-03-16 22:59:31 -0400

    for loop with unknown vaules in array

--- Hello World/Arrays.java
+++ Hello World/Arrays.java
@@ -1,7 +1,14 @@
 public class Arrays {
     public static void main(String[] args) {
-        int [] numbers = new int [5];
-        
-        
+        int[] numbers = new int[5];
+
+        numbers[0] = 4;
+
+        int count = 1; 
+        for (int i = 0; i < numbers.length; i++, count++) {
+            numbers[i] = count;
+            System.out.println(numbers[i]);
+        }
+
     }
 }
