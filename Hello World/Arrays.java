d378fb2e32a3a9d04f207ce0bb071774586fdba6
maclamour <maclamour@gmail.com>
2023-03-16 23:02:44 -0400

    set array with known numbers

--- Hello World/Arrays.java
+++ Hello World/Arrays.java
@@ -1,14 +1,19 @@
 public class Arrays {
     public static void main(String[] args) {
-        int[] numbers = new int[5];
+        // int[] numbers = new int[5];
 
-        numbers[0] = 4;
+        // numbers[0] = 4;
 
-        int count = 1; 
-        for (int i = 0; i < numbers.length; i++, count++) {
-            numbers[i] = count;
-            System.out.println(numbers[i]);
-        }
+        // int count = 1; 
+        // for (int i = 0; i < numbers.length; i++, count++) {
+        //     numbers[i] = count;
+        //     System.out.println(numbers[i]);
+        // }
+
+        int[] numbers = new int[]{1,2,3,4,5};
+        
+        for (int i = 0; i < numbers.length; i++)
+        System.out.println(numbers[i]);
 
     }
 }
