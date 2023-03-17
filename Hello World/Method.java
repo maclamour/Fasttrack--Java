9defaff72be2fb67dd75b7f4e68a2b555af29b0d
maclamour <maclamour@gmail.com>
2023-03-17 09:09:53 -0400

    using methods to call main function

--- Hello World/Method.java
+++ Hello World/Method.java
@@ -1,11 +1,21 @@
 public class Method {
     public static void main(String[] args) {
         System.out.println(add(1,5));
+        System.out.println(concat("cat ", "dog"));
+        System.out.println(divide(6, 6));
 
     }
 
     public static int add(int a, int b) {
         return a + b;
+    }
+
+    public static String concat(String a, String b){
+        return a + b;
+    }
+
+    public static int divide(int numerator, int denominator){
+        return numerator / denominator;
     }
 
 }
