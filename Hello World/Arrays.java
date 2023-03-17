cb3b866bebfe8d2f5360e4186b5ded70f01ce24f
maclamour <maclamour@gmail.com>
2023-03-16 23:13:32 -0400

    creating a new array using a temp array

--- Hello World/Arrays.java
+++ Hello World/Arrays.java
@@ -4,16 +4,30 @@ public class Arrays {
 
         // numbers[0] = 4;
 
-        // int count = 1; 
+        // int count = 1;
         // for (int i = 0; i < numbers.length; i++, count++) {
-        //     numbers[i] = count;
-        //     System.out.println(numbers[i]);
+        // numbers[i] = count;
+        // System.out.println(numbers[i]);
         // }
 
-        int[] numbers = new int[]{1,2,3,4,5};
-        
+        int[] numbers = new int[] { 1, 2, 3, 4, 5};
+
+        int max = (int) Math.random() * 10;
+
+        int currentNumber = 6;
+
+        for (int i = 0; i < max; i++, currentNumber++){
+            int temp[] = new int[numbers.length +1];
+            for (int j = 0; j < numbers.length; j++){
+                temp[j] = numbers[j];
+            }
+            temp[temp.length - 1] = currentNumber;
+            numbers = temp;
+
+        }
+
         for (int i = 0; i < numbers.length; i++)
-        System.out.println(numbers[i]);
+            System.out.println(numbers[i]);
 
     }
 }
