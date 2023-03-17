c0ae834569d1e8c5e85fa2e8a64e81570646c485
maclamour <maclamour@gmail.com>
2023-03-17 09:18:10 -0400

    /** on top of method documents exceptions

--- Hello World/Method.java
+++ Hello World/Method.java
@@ -2,7 +2,7 @@ public class Method {
     public static void main(String[] args) {
         System.out.println(add(1,5));
         System.out.println(concat("cat ", "dog"));
-        System.out.println(divide(6, 6));
+        System.out.println(divide(6, 0));
 
     }
 
@@ -13,9 +13,19 @@ public class Method {
     public static String concat(String a, String b){
         return a + b;
     }
-
-    public static int divide(int numerator, int denominator){
-        return numerator / denominator;
+    
+    /**
+     * Divides two numbers and returns the result
+     * @param a - the number to divide
+     * @param b the number to divide by
+     * @return - the result of dividing a by b
+     * @throws IllegalArgumentException - if b eqauls 0 
+     */
+    public static int divide(int a, int b) throws IllegalArgumentException{
+        if (b == 0){
+            throw new IllegalArgumentException();
+        }
+        return a / b;
     }
 
 }
