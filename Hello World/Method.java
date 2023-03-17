9f07190eb3991a40a72e20ad9504501aedfb8a53
maclamour <maclamour@gmail.com>
2023-03-17 08:58:19 -0400

    add method

---
+++ Hello World/Method.java
@@ -0,0 +1,11 @@
+public class Method {
+    public static void main(String[] args) {
+        System.out.println(add(1,5));
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+}
