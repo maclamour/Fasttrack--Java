732300d1e5ab585e57c620680911077180ba611e
maclamour <maclamour@gmail.com>
2023-03-16 22:14:33 -0400

    else if.java

--- Hello World/HelloWorld.java
+++ Hello World/HelloWorld.java
@@ -3,20 +3,22 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        int totalSugar = 0;
 
-    int totalSugar = 0;
+        int sugar = 7;
 
-    int sugar = 6;
+        int maxSugar = 10;
 
-    int maxSugar = 10;
-
-    if (sugar <= maxSugar){
-        totalSugar += sugar;
-        System.out.println(totalSugar + " tsp's of sugar have been added to you glass!");
-    }else {
-        System.out.println("We only allow " + maxSugar + " tsp's of sugar per glass ");
-    }
-    System.out.println("I run always!");
+        if (sugar < maxSugar) {
+            totalSugar += sugar;
+            System.out.println(totalSugar + " tsp's of sugar have been added to you glass!");
+        } else if (sugar == maxSugar) {
+            totalSugar += maxSugar;
+            System.out.println(totalSugar);
+        } else {
+            System.out.println("We only allow " + maxSugar + " tsp's of sugar per glass ");
+        }
+        System.out.println("I run always!");
 
     }
 
