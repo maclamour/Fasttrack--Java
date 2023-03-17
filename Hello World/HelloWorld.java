4491c08cdc56e4765f6a0dd2306465e9b455710d
maclamour <maclamour@gmail.com>
2023-03-16 22:27:11 -0400

    do while looping

--- Hello World/HelloWorld.java
+++ Hello World/HelloWorld.java
@@ -3,12 +3,14 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        int totalSugar = 0;
+        int totalSugar = 10;
 
         int sugar = 2;
 
         int maxSugar = 10;
 
+        // else/else/else if
+        
         // if (sugar < maxSugar) {
         //     totalSugar += sugar;
         //     System.out.println(totalSugar + " tsp's of sugar have been added to you glass!");
@@ -20,10 +22,18 @@ public class HelloWorld {
         // }
         // System.out.println("I run always!");
 
-        while (totalSugar < maxSugar){
+        // while loop 
+
+        // while (totalSugar < maxSugar){
+        //     totalSugar += sugar;
+        //     System.out.println("Total sugar in glass " + totalSugar);
+        // }
+
+        do {
             totalSugar += sugar;
             System.out.println("Total sugar in glass " + totalSugar);
-        }
+        } while(totalSugar < maxSugar);
+        System.out.println("I don't run until code is finsihed ");
 
     }
 
