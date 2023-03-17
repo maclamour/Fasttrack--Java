3ef3bb914e9b8c3d3ea1c7c474fa8a58e9f81593
maclamour <maclamour@gmail.com>
2023-03-16 22:21:15 -0400

    while loop

--- Hello World/HelloWorld.java
+++ Hello World/HelloWorld.java
@@ -5,20 +5,25 @@ public class HelloWorld {
 
         int totalSugar = 0;
 
-        int sugar = 7;
+        int sugar = 2;
 
         int maxSugar = 10;
 
-        if (sugar < maxSugar) {
+        // if (sugar < maxSugar) {
+        //     totalSugar += sugar;
+        //     System.out.println(totalSugar + " tsp's of sugar have been added to you glass!");
+        // } else if (sugar == maxSugar) {
+        //     totalSugar += maxSugar;
+        //     System.out.println(totalSugar);
+        // } else {
+        //     System.out.println("We only allow " + maxSugar + " tsp's of sugar per glass ");
+        // }
+        // System.out.println("I run always!");
+
+        while (totalSugar < maxSugar){
             totalSugar += sugar;
-            System.out.println(totalSugar + " tsp's of sugar have been added to you glass!");
-        } else if (sugar == maxSugar) {
-            totalSugar += maxSugar;
-            System.out.println(totalSugar);
-        } else {
-            System.out.println("We only allow " + maxSugar + " tsp's of sugar per glass ");
+            System.out.println("Total sugar in glass " + totalSugar);
         }
-        System.out.println("I run always!");
 
     }
 
