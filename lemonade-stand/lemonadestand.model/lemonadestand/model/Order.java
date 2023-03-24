6255a017a8c9eb27eb14571549eed0fe1bcf2706
maclamour <maclamour@gmail.com>
2023-03-24 10:37:42 -0400

    updating order.java

--- lemonade-stand/lemonadestand.model/lemonadestand/model/Order.java
+++ lemonade-stand/lemonadestand.model/lemonadestand/model/Order.java
@@ -1,5 +1,30 @@
 package lemonadestand.model
 
+import java.util.Arrays;
+
 public class Order {
-    
+
+    private Customer customer;
+
+    private Lemonade[] lemonades;
+
+    private double total;
+
+    public Order(Customer customer) {
+        super();
+        this.customer = customer;
+        lemonades = new Lemonade[0];
+    }
+
+    private void updateTotal(){
+        for (Lemonade l: lemonades){
+            total += l.getPrice();
+        }
+    }
+
+    public void addLemonade(Lemonade lemonade) {
+        Lemonade[] temp = Arrays.copyOf(lemonades, lemonades.length + 1);
+        temp[temp.length - 1] = lemonade;
+        lemonades = temp;
+    }
 }
